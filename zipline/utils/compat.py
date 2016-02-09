3aaa211d4799b62808c1a83cfc3cfbe919b15cb8
Joe Jevnik <joe@quantopian.com>
2016-02-08 22:40:00 -0500

    ENH: adds mappingproxy

---
+++ zipline/utils/compat.py
@@ -0,0 +1,11 @@
+from six import PY2
+
+
+if PY2:
+    from ctypes import py_object, pythonapi
+
+    mappingproxy = pythonapi.PyDictProxy_New
+    mappingproxy.argtypes = [py_object]
+    mappingproxy.restype = py_object
+else:
+    from types import MappingProxyType as mappingproxy

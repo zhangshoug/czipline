0dc0365b847aafb6356a269c6292426405bef564
Richard Frank <rich@quantopian.com>
2018-03-05 20:10:32 -0500

    put back upper bound

--- setup.py
+++ setup.py
@@ -171,7 +171,7 @@ def _filter_requirements(lines_iter, filter_names=None,
 
 REQ_UPPER_BOUNDS = {
     'bcolz': '<1',
-    #'pandas': '<0.20',
+    'pandas': '<=0.22',
     'networkx': '<2.0',
 }
 

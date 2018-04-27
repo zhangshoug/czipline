5677232e9d50d495fa6a272f8254bfaabebf82dd
ldf <liu.dengfeng@hotmail.com>
2018-04-27 23:37:36 +0800

    builtin模块基本介绍

--- zipline/pipeline/builtin.py
+++ zipline/pipeline/builtin.py
@@ -1,7 +1,14 @@
 """
 基于基础数据的内置过滤器、分类器
+
+*ST---公司经营连续三年亏损，退市预警。
+ST----公司经营连续二年亏损，特别处理。
+S*ST--公司经营连续三年亏损，退市预警+还没有完成股改。
+SST---公司经营连续二年亏损，特别处理+还没有完成股改。
+S----还没有完成股改
 """
 import numpy as np
+import pandas as pd
 
 from .data.equity_pricing import USEquityPricing
 from .factors import CustomFactor
@@ -9,6 +16,24 @@ from .fundamentals.reader import Fundamentals
 
 
 #============================自定义因子=============================#
+class NDays(CustomFactor):
+    """
+    上市天数
+
+    Notes
+    -----
+    当前日期与上市日期之间自然日历天数    
+    """
+
+    # 输入shape = (m days, n assets)
+    inputs = [Fundamentals.info.asof_date]
+    window_length = 1
+
+    def compute(self, today, assets, out, ts):
+        baseline = today.tz_localize(None)
+        days = [(baseline - pd.Timestamp(x)).days for x in ts[0]]
+        out[:] = days
+
 
 class TradingDays(CustomFactor):
     """
@@ -60,3 +85,26 @@ def QTradableStocksUS():
 
 
 QTradableStocks = QTradableStocksUS
+
+
+def IsNewShare(days=90):
+    """
+    次新股过滤器
+
+    参数
+    _____
+    days：整数
+        上市天数小于指定天数，判定为次新股
+        默认90天
+
+    returns
+    -------
+    zipline.pipeline.Filter
+        次新股过滤器
+
+    备注
+    ----
+        按自然日历计算上市天数
+    """
+    t_days = NDays()
+    return t_days <= days

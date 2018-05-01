846a086cb2c1f6219c231b7440539084f2b7c136
ldf <liu.dengfeng@hotmail.com>
2018-05-01 14:26:41 +0800

    恢复错误注释

--- zipline/tasks.py
+++ zipline/tasks.py
@@ -8,10 +8,12 @@ from zipline.pipeline.fundamentals.static import write_static_info_to_bcolz
 from zipline.pipeline.fundamentals.financal import write_financal_data_to_bcolz
 from zipline.pipeline.fundamentals.dynamic import write_dynamic_data_to_bcolz
 
+
 def main():
-    # write_static_info_to_bcolz()
+    write_static_info_to_bcolz()
     write_financal_data_to_bcolz()
-    # write_dynamic_data_to_bcolz()
+    write_dynamic_data_to_bcolz()
+
 
 if __name__ == '__main__':
     main()

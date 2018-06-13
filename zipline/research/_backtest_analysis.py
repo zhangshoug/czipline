9a6d856823d26cdb9f6640fd06e05932b777b7cc
ldf <liu.dengfeng@hotmail.com>
2018-06-13 13:45:58 +0800

    修改get_backtest函数

--- zipline/research/_backtest_analysis.py
+++ zipline/research/_backtest_analysis.py
@@ -5,21 +5,16 @@ import pandas as pd
 import pyfolio as pf
 
 
-class PrefDataFrame(object):
-    def __init__(self, df):
-        assert isinstance(df, pd.DataFrame), '构造参数df必须为pd.DataFrame'
-        self.data = df
-
-    def create_full_tear_sheet(self):
-        returns, positions, transactions = pf.utils.extract_rets_pos_txn_from_zipline(
-            self.data)
-        live_start_date = self.data.index[-int(len(self.data) / 4)]
-        pf.create_full_tear_sheet(
-            returns,
-            positions=positions,
-            transactions=transactions,
-            live_start_date=live_start_date,
-            round_trips=True)
+def create_full_tear_sheet(self):
+    returns, positions, transactions = pf.utils.extract_rets_pos_txn_from_zipline(
+        self)
+    live_start_date = self.index[-int(len(self) / 4)]
+    pf.create_full_tear_sheet(
+        returns,
+        positions=positions,
+        transactions=transactions,
+        live_start_date=live_start_date,
+        round_trips=True)
 
 
 def get_backtest(dir_name='~/.backtest'):
@@ -28,9 +23,11 @@ def get_backtest(dir_name='~/.backtest'):
     try:
         candidates = os.listdir(pref_root)
         pref_file = os.path.join(pref_root, max(candidates))
-        df = pd.read_pickle(pref_file)
-        return PrefDataFrame(df)
+        return pd.read_pickle(pref_file)
     except (ValueError, OSError) as e:
         if getattr(e, 'errno', errno.ENOENT) != errno.ENOENT:
             raise
         raise ValueError('在目录{}下，没有发现回测结果'.format(dir_name))
+
+
+pd.DataFrame.create_full_tear_sheet = create_full_tear_sheet
